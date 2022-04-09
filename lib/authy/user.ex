11b975c9e1872525d48e3771104500c132c82af4
arturjzapater <zapater.artur@gmail.com>
2022-04-09 11:56:35 +0200

    added password to users

--- lib/authy/user.ex
+++ lib/authy/user.ex
@@ -4,19 +4,35 @@ defmodule Authy.User do
   @primary_key {:id, :binary_id, autogenerate: true}
   @timestamps_opts [type: :utc_datetime_usec]
 
+  @required_for_new [:username, :email, :password]
+  @required_for_change [:username, :email]
+
   schema "users" do
     field :username, :string
     field :email, :string
     field :last_login, :utc_datetime_usec
+    field :password, :string, redact: true
 
     timestamps()
   end
 
-  def changeset(user, params \\ %{}) do
+  def changeset(user, params) do
     user
-    |> Ecto.Changeset.cast(params, [:username, :email, :last_login])
-    |> Ecto.Changeset.validate_required([:username, :email])
+    |> Ecto.Changeset.cast(params, [:username, :email, :last_login, :password])
+    |> Ecto.Changeset.validate_required(get_required(user))
     |> Ecto.Changeset.validate_length(:username, max: 40)
     |> Ecto.Changeset.validate_length(:email, max: 100)
+    |> Ecto.Changeset.unique_constraint([:username, :email])
+    |> hash_password()
   end
+
+  defp get_required(%{id: nil}), do: @required_for_new
+  defp get_required(_user), do: @required_for_change
+
+  defp hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    hash = Bcrypt.hash_pwd_salt(password)
+    Ecto.Changeset.put_change(changeset, :password, hash)
+  end
+
+  defp hash_password(changeset), do: changeset
 end
--- priv/repo/migrations/20220409064140_create_users.exs
+++ priv/repo/migrations/20220409064140_create_users.exs
@@ -4,9 +4,10 @@ defmodule Authy.Repo.Migrations.CreateUsers do
   def change do
     create table(:users, primary_key: false) do
       add :id, :uuid
-      add :username, :string, size: 40, null: false
-      add :email, :string, size: 100, null: false
+      add :username, :string, size: 40, null: false, unique: true
+      add :email, :string, size: 100, null: false, unique: true
       add :last_login, :utc_datetime_usec
+      add :password, :text
 
       timestamps(type: :utc_datetime_usec, default: fragment("now()"))
     end
